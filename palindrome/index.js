bba3ac4ed1881ea68782a4416f748ccf1ac3ccbf
mithhu <mithhu666@gmail.com>
2019-04-20 00:44:59 +0600

    palindrome soln 2

--- palindrome/index.js
+++ palindrome/index.js
@@ -8,12 +8,18 @@
 //   palindrome("abcdefg") === false
 
 /************Sol-1************/
-function palindrome(str) {
-    const reversed = str
-        .split("")
-        .reverse()
-        .join("");
-    return str === reversed;
-}
+// function palindrome(str) {
+//     const reversed = str
+//         .split("")
+//         .reverse()
+//         .join("");
+//     return str === reversed;
+// }
 
+/************Sol-2************/
+function palindrome(str) {
+    return str.split("").every((char, i) => {
+        return char === str[str.length - i - 1];
+    });
+}
 module.exports = palindrome;
